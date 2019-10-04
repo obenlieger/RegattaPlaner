b3bd8a437914ad1435ac05d39d22b88549beb15b
Björn Ohlrich <bjoern.ohlrich@hotmail.de>
2019-10-04 17:17:14 +0200

    Projektdateien hinzufügen.

---
+++ RegattaPlaner/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RegattaPlaner.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
